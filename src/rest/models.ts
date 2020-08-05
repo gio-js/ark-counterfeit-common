4dcbe055a58231a3e06ef371b6931e2329126b54
gio-js <g.sorgente86@gmail.com>
2020-08-05 02:49:51 +0200

    fix on rest model export

--- src/rest/models.ts
+++ src/rest/models.ts
@@ -2,3 +2,4 @@ export { RegisterManufacturerResponse } from './registerManufacturerResponse';
 export { RegisterAccountResponse } from './registerAccountResponse';
 export { RestTransactionContainer }  from './restTransactionContainer';
 export { RestResponse }  from './restResponse';
+export { LoginAccountRequest }  from './loginAccountRequest';
