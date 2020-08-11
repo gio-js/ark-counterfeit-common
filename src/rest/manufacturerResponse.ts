14338d866059403d0563b0eefff6b5d1d39c1b20
gio-js <g.sorgente86@gmail.com>
2020-08-11 09:03:49 +0200

    added ManufacturerResponse

---
+++ src/rest/manufacturerResponse.ts
@@ -0,0 +1,7 @@
+export class ManufacturerResponse {
+    public AddressId: string;
+    public ProductPrefixId: string;
+    public CompanyName: string;
+    public CompanyFiscalCode: string;
+    public RegistrationContract: string;
+}
--- src/rest/models.ts
+++ src/rest/models.ts
@@ -1,5 +1,6 @@
 export { RegisterManufacturerResponse } from './registerManufacturerResponse';
 export { RegisterAccountResponse } from './registerAccountResponse';
-export { RestTransactionContainer }  from './restTransactionContainer';
-export { RestResponse }  from './restResponse';
-export { LoginAccountRequest }  from './loginAccountRequest';
+export { RestTransactionContainer } from './restTransactionContainer';
+export { RestResponse } from './restResponse';
+export { LoginAccountRequest } from './loginAccountRequest';
+export { ManufacturerResponse } from './manufacturerResponse';
